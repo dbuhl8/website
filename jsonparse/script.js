c8a8a6624d46ea8dbfe2ac1ca3d3adf8955b0276
Dante Buhl <dbuhl@ucsc.edu>
2023-05-03 16:37:25 -0700

    ParseJS assignment

---
+++ jsonparse/script.js
@@ -0,0 +1,32 @@
+const data = {
+    "name": "John Doe",
+    "age": 30,
+    "email": "johndoe@example.com",
+    "phone": {
+      "home": "555-1234",
+      "work": "555-5678"
+    },
+    "address": {
+      "street": "123 Main St",
+      "city": "New York",
+      "state": "NY",
+      "zip": "10001"
+    },
+    "education": [
+      {
+        "degree": "Bachelor's",
+        "major": "Computer Science",
+        "school": "University of California, Berkeley",
+        "year": 2014
+      },
+      {
+        "degree": "Master's",
+        "major": "Information Technology",
+        "school": "Stanford University",
+        "year": 2016
+      }
+    ]
+  }
+  
+  console.log(data)
+  console.log('Name:', data.name);
